c1183e0761f073fa44b2849384f4d2f1bbd36eaa
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-07-15 20:16:58 +0200

    toggle on animations

--- src/utils/animationVariants.js
+++ src/utils/animationVariants.js
@@ -2,17 +2,17 @@
 
 export const createOpacityVariants = (number = 1) => {
   const variants = [];
-  // for (let i = 0; i < number; i += 1) {
-  //   variants.push({
-  //     initial: { opacity: 0 },
-  //     animate: {
-  //       opacity: 1,
-  //       transition: {
-  //         duration: 1,
-  //         delay: i * 0.25 + 0.5,
-  //       },
-  //     },
-  //   });
-  // }
+  for (let i = 0; i < number; i += 1) {
+    variants.push({
+      initial: { opacity: 0 },
+      animate: {
+        opacity: 1,
+        transition: {
+          duration: 1,
+          delay: i * 0.25 + 0.5,
+        },
+      },
+    });
+  }
   return variants;
 };
